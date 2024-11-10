690f41d6bb23ae823d9f05b856729b1493af85fc
ryodtabata <ryotabata10@gmail.com>
2024-11-10 15:24:05 -0800

    Save changes before switching to gh-pages

--- src/components/Start.js
+++ src/components/Start.js
@@ -12,7 +12,7 @@ function Start() {
       <h3>
         I was beaten by a friend in chess once. Once. He was a computer science student I met in my first year of 
         university while I was studying Bio-Chemistry. He created a bot that outplayed me. This changed everything. Fast forward 4 years I am about to complete
-        my final term at the University of Victoria with a degree in Computer Sceince; Software Systems. I am very thankful for 
+        my final term at the University of Victoria with a degree in Computer Science; Software Systems. I am very thankful for 
         that chess bot; computer science has allowed me to bring my creative ideas to life to share with all of you.
         
 
